80c224d94b8af4372acabf5b926bf90ba295c682
BuildTools <unconfigured@null.spigotmc.org>
2019-01-19 00:49:06 +0900

    완성

--- RGB셀렉터/Form1.Designer.cs
+++ RGB셀렉터/Form1.Designer.cs
@@ -28,13 +28,204 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.redTextBox = new System.Windows.Forms.TextBox();
+            this.blueTextBox = new System.Windows.Forms.TextBox();
+            this.greenTextBox = new System.Windows.Forms.TextBox();
+            this.redScrollBar = new System.Windows.Forms.HScrollBar();
+            this.greenScrollBar = new System.Windows.Forms.HScrollBar();
+            this.blueScrollBar = new System.Windows.Forms.HScrollBar();
+            this.rgbBox = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.hexTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.rgbBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Red";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Green";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(12, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Blue";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // redTextBox
+            // 
+            this.redTextBox.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.redTextBox.Location = new System.Drawing.Point(168, 9);
+            this.redTextBox.Name = "redTextBox";
+            this.redTextBox.Size = new System.Drawing.Size(150, 32);
+            this.redTextBox.TabIndex = 1;
+            this.redTextBox.Text = "0";
+            this.redTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxValueChange);
+            this.redTextBox.Leave += new System.EventHandler(this.TextBoxFocusOut);
+            // 
+            // blueTextBox
+            // 
+            this.blueTextBox.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.blueTextBox.Location = new System.Drawing.Point(168, 137);
+            this.blueTextBox.Name = "blueTextBox";
+            this.blueTextBox.Size = new System.Drawing.Size(150, 32);
+            this.blueTextBox.TabIndex = 3;
+            this.blueTextBox.Text = "0";
+            this.blueTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxValueChange);
+            this.blueTextBox.Leave += new System.EventHandler(this.TextBoxFocusOut);
+            // 
+            // greenTextBox
+            // 
+            this.greenTextBox.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.greenTextBox.Location = new System.Drawing.Point(168, 76);
+            this.greenTextBox.Name = "greenTextBox";
+            this.greenTextBox.Size = new System.Drawing.Size(150, 32);
+            this.greenTextBox.TabIndex = 2;
+            this.greenTextBox.Text = "0";
+            this.greenTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxValueChange);
+            this.greenTextBox.Leave += new System.EventHandler(this.TextBoxFocusOut);
+            // 
+            // redScrollBar
+            // 
+            this.redScrollBar.Location = new System.Drawing.Point(16, 41);
+            this.redScrollBar.Maximum = 255;
+            this.redScrollBar.Name = "redScrollBar";
+            this.redScrollBar.Size = new System.Drawing.Size(300, 32);
+            this.redScrollBar.TabIndex = 6;
+            this.redScrollBar.ValueChanged += new System.EventHandler(this.ScrollBarValueChanged);
+            // 
+            // greenScrollBar
+            // 
+            this.greenScrollBar.Location = new System.Drawing.Point(16, 105);
+            this.greenScrollBar.Maximum = 255;
+            this.greenScrollBar.Name = "greenScrollBar";
+            this.greenScrollBar.Size = new System.Drawing.Size(300, 32);
+            this.greenScrollBar.TabIndex = 7;
+            this.greenScrollBar.ValueChanged += new System.EventHandler(this.ScrollBarValueChanged);
+            // 
+            // blueScrollBar
+            // 
+            this.blueScrollBar.Location = new System.Drawing.Point(16, 169);
+            this.blueScrollBar.Maximum = 255;
+            this.blueScrollBar.Name = "blueScrollBar";
+            this.blueScrollBar.Size = new System.Drawing.Size(300, 32);
+            this.blueScrollBar.TabIndex = 8;
+            this.blueScrollBar.ValueChanged += new System.EventHandler(this.ScrollBarValueChanged);
+            // 
+            // rgbBox
+            // 
+            this.rgbBox.Location = new System.Drawing.Point(324, 9);
+            this.rgbBox.Name = "rgbBox";
+            this.rgbBox.Size = new System.Drawing.Size(192, 192);
+            this.rgbBox.TabIndex = 9;
+            this.rgbBox.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(12, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 32);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Code";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // hexTextBox
+            // 
+            this.hexTextBox.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.hexTextBox.Location = new System.Drawing.Point(166, 201);
+            this.hexTextBox.Name = "hexTextBox";
+            this.hexTextBox.Size = new System.Drawing.Size(152, 32);
+            this.hexTextBox.TabIndex = 4;
+            this.hexTextBox.Text = "#000000";
+            this.hexTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxValueChange);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox1.Image = global::RGB셀렉터.Properties.Resources.eyedropper;
+            this.pictureBox1.Location = new System.Drawing.Point(485, 207);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 29);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(526, 248);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.hexTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rgbBox);
+            this.Controls.Add(this.blueScrollBar);
+            this.Controls.Add(this.greenScrollBar);
+            this.Controls.Add(this.redScrollBar);
+            this.Controls.Add(this.greenTextBox);
+            this.Controls.Add(this.blueTextBox);
+            this.Controls.Add(this.redTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RGBSelector";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.rgbBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox redTextBox;
+        private System.Windows.Forms.TextBox blueTextBox;
+        private System.Windows.Forms.TextBox greenTextBox;
+        private System.Windows.Forms.HScrollBar redScrollBar;
+        private System.Windows.Forms.HScrollBar greenScrollBar;
+        private System.Windows.Forms.HScrollBar blueScrollBar;
+        private System.Windows.Forms.PictureBox rgbBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox hexTextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- RGB셀렉터/Form1.cs
+++ RGB셀렉터/Form1.cs
@@ -1,20 +1,310 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace RGB셀렉터
 {
     public partial class Form1 : Form
     {
+        public static bool spoidMode = false;
         public Form1()
         {
             InitializeComponent();
+
+            MouseHook.Start();
+            MouseHook.MouseUpAction += new EventHandler(MouseUpEvent);
+            MouseHook.MouseMoveAction += new EventHandler(MouseMoveEvent);
         }
+
+        void MouseUpEvent(object sender, EventArgs e)
+        {
+            if (!spoidMode) return;
+            Cursor.Current = Cursors.Default;
+            spoidMode = false;
+        }
+
+        void MouseMoveEvent(object sender, EventArgs e)
+        {
+            if (!spoidMode) return;
+            var value = sender as Point;
+            SetColorValue(new RGB(GetScreenColor(value.x, value.y)));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetColorValue(new RGB(200, 200, 200));
+        }
+
+        void SetColorValue(RGB rgb)
+        {
+            redTextBox.Text = rgb.red.ToString();
+            redScrollBar.Value = rgb.red;
+
+            greenTextBox.Text = rgb.green.ToString();
+            greenScrollBar.Value = rgb.green;
+
+            blueTextBox.Text = rgb.blue.ToString();
+            blueScrollBar.Value = rgb.blue;
+
+            hexTextBox.Text = rgb.ToString();
+
+            rgbBox.BackColor = rgb.ToColor();
+        }
+
+        RGB ChangeRGB(string control)
+        {
+            var rgb = new RGB(rgbBox.BackColor);
+            try
+            {
+                switch (control)
+                {
+                    case "redTextBox":
+                        rgb.red = Convert.ToInt32(redTextBox.Text); break;
+                    case "redScrollBar":
+                        rgb.red = redScrollBar.Value; break;
+                    case "greenTextBox":
+                        rgb.green = Convert.ToInt32(greenTextBox.Text); break;
+                    case "greenScrollBar":
+                        rgb.green = greenScrollBar.Value; break;
+                    case "blueTextBox":
+                        rgb.blue = Convert.ToInt32(blueTextBox.Text); break;
+                    case "blueScrollBar":
+                        rgb.blue = blueScrollBar.Value; break;
+                    case "hexTextBox":
+                        rgb = new RGB(hexTextBox.Text); break;
+                }
+            }
+            catch
+            {
+                rgb = new RGB(rgbBox.BackColor);
+            }
+
+            return rgb;
+        }
+
+        Color GetScreenColor(int x, int y)
+        {
+            Bitmap bmp = new Bitmap(1, 1);
+            Graphics graphics = Graphics.FromImage(bmp);
+            graphics.CopyFromScreen(x, y, 0, 0, new Size(1, 1));
+
+            return bmp.GetPixel(0, 0);
+        }
+
+        private void TextBoxValueChange(object sender, KeyEventArgs e)
+        {
+            var control = sender as TextBox;
+            if (e.KeyCode == Keys.Enter)
+            {
+                var rgb = new RGB(rgbBox.BackColor);
+                try
+                {
+                    SetColorValue(ChangeRGB(control.Name));
+                }
+                catch
+                {
+                    SetColorValue(rgb);
+                }
+            }
+        }
+
+        private void ScrollBarValueChanged(object sender, EventArgs e)
+        {
+            var control = sender as HScrollBar;
+
+            SetColorValue(ChangeRGB(control.Name));
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) { String temp = ""; temp = String.Format("x : {0} y : {1}", e.X, e.Y); Console.WriteLine(temp); }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            spoidMode = true;
+            Cursor.Current = Cursors.Cross;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MouseHook.stop();
+        }
+
+        private void TextBoxFocusOut(object sender, EventArgs e)
+        {
+            var control = sender as TextBox;
+            var rgb = new RGB(rgbBox.BackColor);
+            try
+            {
+                SetColorValue(ChangeRGB(control.Name));
+            }
+            catch
+            {
+                SetColorValue(rgb);
+            }
+        }
+    }
+
+    class RGB
+    {
+        #region 변수
+        public int red { get; set; }
+        public int green { get; set; }
+        public int blue { get; set; }
+        #endregion
+
+        #region 생성자들
+        public RGB()
+        {
+            red = 0;
+            green = 0;
+            blue = 0;
+        }
+        public RGB(int red, int green, int blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+        public RGB(string hexString)
+        {
+            hexString = hexString.Replace("#", "").Trim(' ');
+
+            red = Convert.ToInt32(hexString.Substring(0, 2), 16);
+            green = Convert.ToInt32(hexString.Substring(2, 2), 16);
+            blue = Convert.ToInt32(hexString.Substring(4, 2), 16);
+            red = green = blue = 0;
+        }
+        public RGB(Color color)
+        {
+            red = color.R;
+            green = color.G;
+            blue = color.B;
+        }
+        #endregion
+
+        #region 변환
+        override public string ToString()
+        {
+            return string.Format("#{0}{1}{2}",
+                red.ToString("X2"),
+                green.ToString("X2"),
+                blue.ToString("X2"));
+        }
+        public Color ToColor()
+        {
+            return Color.FromArgb(red, green, blue);
+        }
+        #endregion
+    }
+
+    class Point
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static class MouseHook
+    {
+        public static event EventHandler MouseUpAction = delegate { };
+        public static event EventHandler MouseMoveAction = delegate { };
+
+        public static void Start()
+        {
+            _hookID = SetHook(_proc);
+
+
+        }
+        public static void stop()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static LowLevelMouseProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static IntPtr SetHook(LowLevelMouseProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc,
+                  GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(
+          int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (!Form1.spoidMode) return CallNextHookEx(_hookID, nCode, wParam, lParam);
+            if (nCode >= 0 && MouseMessages.WM_LBUTTONUP == (MouseMessages)wParam)
+            {
+                MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                MouseUpAction(null, new EventArgs());
+            }
+            else if (nCode >= 0 && MouseMessages.WM_MOUSEMOVE == (MouseMessages)wParam)
+            {
+                MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                MouseMoveAction(new Point(hookStruct.pt.x, hookStruct.pt.y), new EventArgs());
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private const int WH_MOUSE_LL = 14;
+
+        private enum MouseMessages
+        {
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_MOUSEMOVE = 0x0200,
+            WM_MOUSEWHEEL = 0x020A,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MSLLHOOKSTRUCT
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+          LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+          IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+
     }
 }
